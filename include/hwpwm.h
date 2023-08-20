567e5645f007cdcb1d116e229e8d928a7cd8012e
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-08-20 15:47:53 +0200

    hwpwm: Fix license notice

--- include/hwpwm.h
+++ include/hwpwm.h
@@ -11,7 +11,7 @@
  * libhwpwm is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with libhwpwm. If not, see <https://www.gnu.org/licenses/>.
